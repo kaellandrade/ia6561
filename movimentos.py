434aea9b694ccdde963c79c671fcda18735503e6
Gilberto da Mota Junior <gilbertomj@dcomp.ufs.br>
2023-03-09 15:03:38 -0300

    Criado movimentos para esquerda e direita

--- movimentos.py
+++ movimentos.py
@@ -1,24 +1,51 @@
-lista = [1,0,1,0]
+def retiraEspacosVazios(lista, movimento):
+    match movimento:
+        case 'L':
+            j = 0
+            for i in range(len(lista)):
+                if lista[i] == 0:
+                    j+=1
+                elif lista[i-j] == 0:
+                    lista[i-j] = lista[i]
+                    lista[i] = 0
+        case 'R':
+            j = 0
+            for i in range(len(lista),0,-1):
+                if lista[i-1] == 0:
+                    j+=1
+                elif lista[i+j-1] == 0:
+                    lista[i+j-1] = lista[i-1]
+                    lista[i-1] = 0
 
-def retiraEspacosVazios(lista):
-    j = 0
-    for i in range(len(lista)):
-        if lista[i] == 0:
-            j+=1
-        elif lista[i-j] == 0:
-            lista[i-j] = lista[i]
-            lista[i] = 0
 
-def somaPosicao(lista):
-    retiraEspacosVazios(lista)
-    i = 1
-    while i < len(lista):
-        if lista[i-1] == 1:
-            if lista[i-1] == lista[i]:
-                lista[i-1] = 3
-                lista[i] = 0
-        i+=1
-    retiraEspacosVazios(lista)
+def somaPosicao(lista, movimento):
+    retiraEspacosVazios(lista, movimento)
+    match movimento:
+        case 'L':
+            i = 1
+            while i < len(lista):
+                if lista[i-1] != 0:
+                    if lista[i-1] == lista[i]:
+                        lista[i-1] = 3*lista[i]
+                        lista[i] = 0
+                i+=1
+        case 'R':
+            i = 1
+            while i < len(lista):
+                if lista[len(lista)-i] != 0:
+                    if lista[len(lista)-1-i] == lista[len(lista)-i]:
+                        lista[len(lista)-i] = 3*lista[len(lista)-i-1]
+                        lista[len(lista)-i-1] = 0
+                i+=1
+
+    retiraEspacosVazios(lista, movimento)
     print(lista)
 
-somaPosicao(lista)
+lista1 = [27,9,3,27,9,9,1,3,3,1,9,9,9,27]
+somaPosicao(lista1,'L')
+
+lista2 = [27,9,3,27,9,9,1,3,3,1,9,9,0,27]
+somaPosicao(lista2, 'R')
+
+#for i in range(len(lista), 0, -1):
+#    print(lista[i-1])
