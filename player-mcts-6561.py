796b747f69fc40b023cfb1b36be5b70f72a29fb1
Micael Andrade Dos Santos <micaelandrade@moobitech.com.br>
2023-03-11 15:20:44 -0300

    Refatorando. Documentando funções. Torando os nós com valor inicial igual a 0.

--- player-mcts-6561.py
+++ player-mcts-6561.py
@@ -9,6 +9,7 @@ import copy
 COR_VERMELHO = 'vermelho'
 COR_AZUL = 'azul'
 COR_CINZA = 'cinza'
+COR_BRANCO = 'branco'
 
 PARA_DIREITA = 'R'
 PARA_ESQUERDA = 'L'
@@ -19,10 +20,9 @@ cores = {
     COR_VERMELHO: Back.RED,
     COR_AZUL: Back.BLUE,
     COR_CINZA: Back.BLACK,
-    'branco': Back.WHITE  # Não tem no jogo (controle interno da estrutura)
+    COR_BRANCO: Back.WHITE  # Branco não tem no jogo (controle interno da estrutura)
 }
-VALOR_INICIAL_NO = 1  # Valor que irá ser dado em cada rodada
-VALORES_INICIAIS_NO = [1, 3, 9, 27, None]  # Lista para criar o tabuleiro
+VALOR_INICIAL_POR_RODADA = 1
 RITMO_DO_JOGO = ['azul', 'vermelho', 'cinza', 'deslizar', 'deslizar']
 
 
@@ -48,10 +48,10 @@ class No:
     """
     Classe que representa um No em um tabuleiro. (Vazio ou não)
     """
-    valor = None   #TODO: refatorar isso para 0
-    cor = 'branco'
+    valor = 0
+    cor = COR_BRANCO
 
-    def __init__(self, valor=None, cor='branco'):
+    def __init__(self, valor=0, cor=COR_BRANCO):
         """
         Construtor da classe Nó
         :param valor: Valor do nó
@@ -70,32 +70,38 @@ class No:
         return '  '
 
     def isNoVazio(self):
-        return self.valor is None
+        """
+        Verifica se um dado nó está vazio (com valor 0).
+        """
+        return self.valor == 0
 
     def getValor(self):
+        """
+        Retorna o valor do Nó
+        """
         return self.valor
 
-    def getValor2(self):
-        if self.isNoVazio():
-            return 0
-        return self.getValor()
-
     def getCor(self):
+        """
+        Retorna a cor do Nó.
+        """
         return self.cor
 
     def setValor(self, valor):
+        """
+        Seta o valor do Nó.
+        """
         self.valor = valor
 
     def isMesmaConfiguracao(self, no2):
         """
-        Verifica se um dado nó possui a mesma configuração que o nó atual.
+        Verifica se um dado Nó possui a mesma configuração que o nó atual.
         """
         return self.getCor() == no2.getCor() and self.getValor() == no2.getValor()
 
 
 class Tabuleiro:
     """
-    Classe que representa o meu tabuleiro.
     """
     matriz = []
     dimensao = 4
@@ -104,8 +110,9 @@ class Tabuleiro:
     def __init__(self, dimensao=4, inicializarNos=True):
         """
         Construtor da Classe Tabuleiro.
-        Inicia meu tabuleiro com todos os Nós vazios.
+        Inicia meu tabuleiro com todos os Nós vazios (Por padrão).
         :param dimensao: Dimensão do meu tabuleiro (4 por padrão)
+        :param inicializarNos: Inicializar os nós do tabuleiro.
         """
         self.dimensao = dimensao
         if inicializarNos:
@@ -131,6 +138,7 @@ class Tabuleiro:
     def _iniciarTabuleiro(self):
         """
         Inicia o tabuleiro com todos os Nós vazios.
+        Caso essa função não seja executada a matriz estará vazia []
         :return: Void
         """
         for i in range(self.dimensao):
@@ -151,8 +159,7 @@ class Tabuleiro:
         novoNo = No()
         novoNo.cor = cor
         novoNo.valor = valor
-        novoNo.score = 0
-        linhaParaInserir, colunaParaInserir = self._splitCoordenada(coordenada)
+        linhaParaInserir, colunaParaInserir = self._splitCoordenada(coordenada) # Destructuring
         self.matriz[linhaParaInserir][colunaParaInserir] = novoNo
         self.calcularScoreTabuleiro()
 
@@ -168,7 +175,7 @@ class Tabuleiro:
 
     def _splitCoordenada(self, coordenada):
         """
-        Recebe uma coordenada xx(padrão do jogo) e retorna uma coordenada válida para a matriz do tabuleiro.
+        Recebe uma coordenada xx(padrão do caia) e retorna uma coordenada válida para a matriz do tabuleiro.
         :param coordenada:
         :return: Void
         """
@@ -182,8 +189,8 @@ class Tabuleiro:
         :param coordenada:
         :return: Booleano
         """
-        linhaParaInserir, colunaParaInserir = self._splitCoordenada(coordenada)
-        if self.matriz[linhaParaInserir][colunaParaInserir].valor is None:
+        linhaParaInserir, colunaParaInserir = self._splitCoordenada(coordenada) #Destructuring
+        if not bool(self.matriz[linhaParaInserir][colunaParaInserir].valor):
             return True
         else:
             return False
@@ -195,23 +202,23 @@ class Tabuleiro:
         :param movimento: R, L
         :return: Void
         """
-        match movimento:
-            case 'L':  # De baixo para cima
-                j = 0
-                for i in range(len(lista)):
-                    if lista[i].isNoVazio():
-                        j += 1
-                    elif lista[i - j].isNoVazio():
-                        lista[i - j] = lista[i]
-                        lista[i] = No()
-            case 'R':  # De cima para baixo
-                j = 0
-                for i in range(len(lista), 0, -1):
-                    if lista[i - 1].isNoVazio():
-                        j += 1
-                    elif lista[i + j - 1].isNoVazio():
-                        lista[i + j - 1] = lista[i - 1]
-                        lista[i - 1] = No()
+        if PARA_ESQUERDA == movimento:  # De baixo para cima
+            j = 0
+            for i in range(len(lista)):
+                if lista[i].isNoVazio():
+                    j += 1
+                elif lista[i - j].isNoVazio():
+                    lista[i - j] = lista[i]
+                    lista[i] = No()
+
+        if PARA_DIREITA == movimento:  # De cima para baixo
+            j = 0
+            for i in range(len(lista), 0, -1):
+                if lista[i - 1].isNoVazio():
+                    j += 1
+                elif lista[i + j - 1].isNoVazio():
+                    lista[i + j - 1] = lista[i - 1]
+                    lista[i - 1] = No()
 
     def _aplicarRegraLinha(self, lista, movimento):
         """
@@ -222,36 +229,34 @@ class Tabuleiro:
         """
         self._isolarEspacosVazios(lista, movimento)
         TAMANHO_LISTA = len(lista)
-        match movimento:
-            case 'L':
-                i = 1
-                while i < TAMANHO_LISTA:
-                    if not lista[i - 1].isNoVazio():
-                        noA = lista[i - 1]
-                        noB = lista[i]
-                        # Mesclando peças (mesma cor e mesmo valor)
-                        if noA.getValor() == noB.getValor() and noA.getCor() == noB.getCor():
-                            lista[i - 1].setValor(3 * lista[i].getValor())
-                            lista[i] = No()
-                        # Limpando as casas (Mesmo valor, porém com cores distintas)
-                        if noA.getValor() == noB.getValor() and noA.getCor() != noB.getCor():
-                            lista[i-1] = No()
-                            lista[i] = No()
-                    i += 1
-            case 'R':
-                i = 1
-                while i < TAMANHO_LISTA:
-                    noA = lista[TAMANHO_LISTA - i]
-                    noB = lista[TAMANHO_LISTA - 1 - i]
-                    if not noA.isNoVazio():
-                        if noA.getValor() == noB.getValor() and noA.getCor() == noB.getCor():
-                            noA.setValor(3 * noB.getValor())
-                            lista[TAMANHO_LISTA - i - 1] = No()
-                        if noA.getValor() == noB.getValor() and noA.getCor() != noB.getCor():
-                            lista[TAMANHO_LISTA - i] = No()
-                            lista[TAMANHO_LISTA - 1 - i] = No()
-                    i += 1
-
+        if movimento == PARA_ESQUERDA:
+            i = 1
+            while i < TAMANHO_LISTA:
+                if not lista[i - 1].isNoVazio():
+                    noA = lista[i - 1]
+                    noB = lista[i]
+                    # Mesclando peças (mesma cor e mesmo valor)
+                    if noA.getValor() == noB.getValor() and noA.getCor() == noB.getCor():
+                        lista[i - 1].setValor(3 * lista[i].getValor())
+                        lista[i] = No()
+                    # Limpando as casas (Mesmo valor, porém com cores distintas)
+                    if noA.getValor() == noB.getValor() and noA.getCor() != noB.getCor():
+                        lista[i-1] = No()
+                        lista[i] = No()
+                i += 1
+        if movimento == PARA_DIREITA:
+            i = 1
+            while i < TAMANHO_LISTA:
+                noA = lista[TAMANHO_LISTA - i]
+                noB = lista[TAMANHO_LISTA - 1 - i]
+                if not noA.isNoVazio():
+                    if noA.getValor() == noB.getValor() and noA.getCor() == noB.getCor():
+                        noA.setValor(3 * noB.getValor())
+                        lista[TAMANHO_LISTA - i - 1] = No()
+                    if noA.getValor() == noB.getValor() and noA.getCor() != noB.getCor():
+                        lista[TAMANHO_LISTA - i] = No()
+                        lista[TAMANHO_LISTA - 1 - i] = No()
+                i += 1
         self._isolarEspacosVazios(lista, movimento)
 
     def _transporMatriz(self, matriz):
@@ -302,12 +307,12 @@ class Tabuleiro:
             self._aplicarRegraLinha(self.matriz[i], PARA_ESQUERDA)
         self._transporMatriz(self.matriz)                              # Retornar para a matriz original
 
-    def isDesLizeValido(self, movimentoDeDeslize):
+    def isDeslizeValido(self, movimentoDeDeslize):
         """
         Verifica se um movimento de deslize é válido.
         Caso o deslize não mude a configuração do tabuleiro, esse deslize é inválido.
         """
-        novoTabuleiro = Tabuleiro(4, False)  # Não quero iniciar os nós, pois irei pegar do tabuleiro original.
+        novoTabuleiro = Tabuleiro(4, False)  # Não quero iniciar os nós, pois irei pegar do tabuleiro original. (matriz= [])
         copiaDaMatrizOriginal = self.getCopiaDaMatriz()
         novoTabuleiro.setMatrizNos(copiaDaMatrizOriginal)
         novoTabuleiro.deslizar(movimentoDeDeslize)
@@ -334,6 +339,7 @@ class Tabuleiro:
         """
         Desliza o tabuleiro para uma das quatro direções.
         :return: Void
+        :lado: PARA_DIREITA, PARA_ESQUERDA, PARA_BAIXO e PARA_CIMA
         """
         self.MOVIMENTOS.get(lado)()
         self.calcularScoreTabuleiro()
@@ -341,11 +347,11 @@ class Tabuleiro:
 
     def calcularScoreTabuleiro(self):
         """
-        Função para calcular a pontuação atual do tabuleiro: somar o atributo valor de todos os nós atuais do tabuleiro.
-        :return:
+        Função para calcular a pontuação atual do tabuleiro: somar o atributo valor de todos os nós do tabuleiro.
+        :return: Void
         """
         listaNos = list(chain(*self.matriz))
-        self.scoreAtualTabuleiro = reduce(lambda atual, prox: atual + prox.getValor2(), listaNos, 0)
+        self.scoreAtualTabuleiro = reduce(lambda atual, prox: atual + prox.getValor(), listaNos, 0)
 
 
     def getScoreTabuleiro(self):
@@ -365,6 +371,10 @@ class Game:
         self.tabuleiro = tabuleiro
 
     def getTabuleiro(self):
+        """
+        Retorna o tabuleiro do jogo.
+        :return: Tabuleiro()
+        """
         return self.tabuleiro
 
     def getAcaoPorRodada(self, rodada):
@@ -372,22 +382,23 @@ class Game:
         Função que, através da rodada atual, descobre qual deve ser a minha jogada.
         Ex: estando na rodada x, devo colocar uma peça azul, vermelha, cinza, ou deslizar o tabuleiro?
         :param rodada:
-        :return:
+        :return: String -> 'azul', 'vermelho', 'cinza', 'deslizar' ou 'deslizar'
         """
         return RITMO_DO_JOGO[(rodada - 1) % len(RITMO_DO_JOGO)]
 
     def getCordenadaAleatoria(self):
         """
         Função para escolher aleatoriamente uma posição do tabuleiro para o jogador jogar.
-        :return:
+        :return: Int -> 11, 12, 13, 14, 21, 22, 23, 24, 31, 32, 33, 34, 41, 42, 43 ou 44
         """
         return random.choice(CORDENADAS_TABULEIRO)
 
     def calcularScoreJogo(self):
         """
-        Função para atualizar a pontuação máxima durante todo o jogo. Ou seja, a maior pontuação do tabuleiro.
+        Função para atualizar a pontuação máxima durante todo o jogo.
+        Ou seja, a maior pontuação do tabuleiro.
         Essa pontuação representa a pontuação que será dada aos jogadores.
-        :return:
+        :return: Int
         """
         scoreTabuleiro = self.tabuleiro.getScoreTabuleiro()
         if scoreTabuleiro > self.scoreMaxJogo:
@@ -396,7 +407,7 @@ class Game:
     def getScoreJogo(self):
         """
         Função para retornar a pontuação máxima do jogo.
-        :return:
+        :return: Int
         """
         return self.scoreMaxJogo
 
@@ -419,7 +430,7 @@ def runCaia():
                 coordenada = game.getCordenadaAleatoria()
                 while not game.getTabuleiro().hasPosicaoVazio(coordenada):
                     coordenada = game.getCordenadaAleatoria()
-                game.getTabuleiro().inserirNoPorCoordenada(coordenada, VALOR_INICIAL_NO, game.getAcaoPorRodada(rodada))
+                game.getTabuleiro().inserirNoPorCoordenada(coordenada, VALOR_INICIAL_POR_RODADA, game.getAcaoPorRodada(rodada))
                 print(coordenada)
                 sys.stdout.flush()
             else:
@@ -427,7 +438,7 @@ def runCaia():
                 deslizesPossiveis = list(game.getTabuleiro().MOVIMENTOS.keys())
                 comandoDeDeslize = random.choice(deslizesPossiveis)
 
-                while not game.getTabuleiro().isDesLizeValido(comandoDeDeslize):
+                while not game.getTabuleiro().isDeslizeValido(comandoDeDeslize):
                     comandoDeDeslize = random.choice(list(game.getTabuleiro().MOVIMENTOS.keys()))
 
                 game.getTabuleiro().deslizar(comandoDeDeslize)
@@ -451,8 +462,8 @@ def runCaia():
 
             if entrada.strip() != "Quit" and game.getAcaoPorRodada(rodada - 1) != 'deslizar':
                 coordenadaDoOponente = int(entrada.strip())
-                game.getTabuleiro().inserirNoPorCoordenada(coordenadaDoOponente, VALOR_INICIAL_NO,
-                                                 game.getAcaoPorRodada(rodada - 1))
+                game.getTabuleiro().inserirNoPorCoordenada(coordenadaDoOponente, VALOR_INICIAL_POR_RODADA,
+                                                           game.getAcaoPorRodada(rodada - 1))
 
     else:
         # jogando como jogador B
@@ -474,8 +485,8 @@ def runCaia():
 
             if entrada.strip() != "Quit" and game.getAcaoPorRodada(rodada - 1) != 'deslizar':
                 coordenadaDoOponente = int(entrada.strip())
-                game.getTabuleiro().inserirNoPorCoordenada(coordenadaDoOponente, VALOR_INICIAL_NO,
-                                                 game.getAcaoPorRodada(rodada - 1))
+                game.getTabuleiro().inserirNoPorCoordenada(coordenadaDoOponente, VALOR_INICIAL_POR_RODADA,
+                                                           game.getAcaoPorRodada(rodada - 1))
 
             # Realizando minha jogada
             if entrada.strip() == "Quit":
@@ -484,20 +495,18 @@ def runCaia():
                 coordenada = game.getCordenadaAleatoria()
                 while not game.getTabuleiro().hasPosicaoVazio(coordenada):
                     coordenada = game.getCordenadaAleatoria()
-                game.getTabuleiro().inserirNoPorCoordenada(coordenada, VALOR_INICIAL_NO, game.getAcaoPorRodada(rodada))
+                game.getTabuleiro().inserirNoPorCoordenada(coordenada, VALOR_INICIAL_POR_RODADA, game.getAcaoPorRodada(rodada))
                 print(coordenada)
                 sys.stdout.flush()
             else:
                 comandoDeDeslize = random.choice(list(game.getTabuleiro().MOVIMENTOS.keys()))
-                while not game.getTabuleiro().isDesLizeValido(comandoDeDeslize):
+                while not game.getTabuleiro().isDeslizeValido(comandoDeDeslize):
                     comandoDeDeslize = random.choice(list(game.getTabuleiro().MOVIMENTOS.keys()))
 
                 game.getTabuleiro().deslizar(comandoDeDeslize)
                 print(comandoDeDeslize)
                 sys.stdout.flush()
             rodada += 2
-
-
 
 
 def runLocal():
@@ -508,6 +517,7 @@ def runLocal():
     game = Game(Tabuleiro())
     print('Configuração inicial')
     game.getTabuleiro().printTabuleiro()
+
     game.getTabuleiro().inserirNoPorCoordenada(43, 1, COR_AZUL)
     game.getTabuleiro().inserirNoPorCoordenada(34, 1, COR_VERMELHO)
     game.getTabuleiro().inserirNoPorCoordenada(13, 1, COR_CINZA)
